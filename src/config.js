ae0a33a00d93fc87ca674a5f48f8ab1d4c0e660f
Interodex <109031223+interoPlat@users.noreply.github.com>
2022-08-03 21:28:13 +0500

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,7 @@
+import { createAlchemyWeb3 } from '@alch/alchemy-web3';
+export const NFTCONTRACT = "0xfA3Bcfe820dF123f13650d34bCc5ab1Ad2213FBC";
+export const STAKINGCONTRACT = "0xC76CCdD0Fc0393B135BeBa8e1f7e8515bA842c29";
+export const polygonscanapi = "https://api.polygonscan.com/api";
+export const moralisapi = "https://deep-index.moralis.io/api/v2/";
+export const nftpng = "https://ipfs.io/ipfs/QmavM8Zpo9bD3r4zEnhbbBLLvHyfr1YL7f1faG3ovaeSSG/";
+export const Web3Alc = createAlchemyWeb3("https://polygon-mainnet.g.alchemy.com/v2/LLAbWRfbb6CllzU_vTOBUsY57jnJvR2O");
